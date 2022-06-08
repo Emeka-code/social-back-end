8567ee46032a114c63f5c22bdd87d3c2ea6e038b
Emeka-code <clintonekegbu@gmail.com>
2022-06-08 16:37:08 +0100

    backend

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 5000;
+require("./utils/db");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  try {
+    res.status(200).json({ message: "Awesome, now connecetd" });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+});
+
+app.use("/api/user", require("./router/userRouter"));
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
